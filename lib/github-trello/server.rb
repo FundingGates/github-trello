c118d134e62819e1340d3f16b025c5fbbc5895f8
Katie Gengler <katie@kmg.io>
2013-10-02 23:17:20 -0400

    Limit integration to master

--- lib/github-trello/server.rb
+++ lib/github-trello/server.rb
@@ -23,43 +23,45 @@ module GithubTrello
       #Get branch
       branch = payload["ref"].gsub("refs/heads/", "")
 
-      payload["commits"].each do |commit|
-        # Figure out the card short id
-        match = commit["message"].match(/((start|per|finish|fix)e?s? \D?([0-9]+))/i)
-        next unless match and match[3].to_i > 0
+      if branch == 'master'
+        payload["commits"].each do |commit|
+          # Figure out the card short id
+          match = commit["message"].match(/((start|per|finish|fix)e?s? \D?([0-9]+))/i)
+          next unless match and match[3].to_i > 0
 
-        results = http.get_card(board_id, match[3].to_i)
-        unless results
-          puts "[ERROR] Cannot find card matching ID #{match[3]}"
-          next
-        end
+          results = http.get_card(board_id, match[3].to_i)
+          unless results
+            puts "[ERROR] Cannot find card matching ID #{match[3]}"
+            next
+          end
 
-        results = JSON.parse(results)
+          results = JSON.parse(results)
 
-        # Add the commit comment
-        message = "#{commit["author"]["name"]}: #{commit["message"]}\n\n[#{branch}] #{commit["url"]}"
-        message.gsub!(match[1], "")
-        message.gsub!(/\(\)$/, "")
+          # Add the commit comment
+          message = "#{commit["author"]["name"]}: #{commit["message"]}\n\n[#{branch}] #{commit["url"]}"
+          message.gsub!(match[1], "")
+          message.gsub!(/\(\)$/, "")
 
-        http.add_comment(results["id"], message)
+          http.add_comment(results["id"], message)
 
-        #Determine the action to take
-        new_list_id = case match[2].downcase
-         when "start", "per" then start_list_target_id
-         when "finish", "fix" then finish_list_target_id
-        end
+          #Determine the action to take
+          new_list_id = case match[2].downcase
+           when "start", "per" then start_list_target_id
+           when "finish", "fix" then finish_list_target_id
+          end
 
-        next unless !new_list_id.nil?
-        
-        #Modify it if needed
-        to_update = {}
+          next unless !new_list_id.nil?
 
-        unless results["idList"] == new_list_id
-         to_update[:idList] = new_list_id
-        end
+          #Modify it if needed
+          to_update = {}
 
-        unless to_update.empty?
-         http.update_card(results["id"], to_update)
+          unless results["idList"] == new_list_id
+           to_update[:idList] = new_list_id
+          end
+
+          unless to_update.empty?
+           http.update_card(results["id"], to_update)
+          end
         end
       end
       ""
